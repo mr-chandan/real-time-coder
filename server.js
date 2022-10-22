c38d10525cdd25dc703ec986c3e66b57b51c5ef2
chandan <chandanchethan321@gmail.com>
2022-10-22 13:17:09 +0530

    feature

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const actions = require('./src/Actions');
+const io = new Server(server);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const userSocketMap = {};
+
+function getallclienta(roomid) {
+  return Array.from(io.sockets.adapter.room.get(roomid) || [].map((socketId) => {
+    return {
+      socketId,
+      username: userSocketMap[socketId]
+    }
+  }));
+}
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on(actions.JOIN, ({ roomid, username }) => {
+
+    userSocketMap[socket.id] = username;
+    socket.join(roomid);
+    const clients = getallclienta(roomid);
+
+    clients.forEach(({ socketId }) => {
+      io.to(socketId.emit(actions.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      }))
+    })
+
+  })
+});
+
+server.listen(5000, () => {
+  console.log('listening on :5000');
+});
